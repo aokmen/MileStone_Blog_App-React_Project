eca276647e08972bb011c247e55aa3a27471613f
aokmen <abdullah.okmen.11@gmail.com>
2023-09-19 21:32:49 +0200

    files_edited

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { Provider } from "react-redux";
 import store from "./app/store";
 import AppRouter from "./router/AppRouter";
+import {ToastContainer} from "react-toastify"
 import  "./App.css"
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
+
   return (
     <>
       <Provider store={store}>
           <AppRouter/>
        </Provider>
+       <ToastContainer/>   
     </>
   );
 }
