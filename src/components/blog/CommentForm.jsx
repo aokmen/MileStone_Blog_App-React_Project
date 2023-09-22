80bdf5742b4f6a99556101a5815ef005a3f70bdb
aokmen <abdullah.okmen.11@gmail.com>
2023-09-22 16:36:49 +0200

    Form_Comment_updated

--- src/components/blog/CommentForm.jsx
+++ src/components/blog/CommentForm.jsx
@@ -1,18 +1,40 @@
 import { Button, FormControl, TextField } from '@mui/material'
 import React from 'react'
 import InputAdornment from '@mui/material/InputAdornment';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import useBlogCalls from '../../hooks/useBlogCalls';
+import { useParams } from 'react-router-dom';
+
+
 const CommentForm = () => {
-    
+  const {postComment} = useBlogCalls()
+  const {id} = useParams()
+  const [info, setInfo] = React.useState({
+    post: id,
+    content: ""
+  });
+
+
+const handleChange = (e) => {setInfo({...info,[e.target.name]:e.target.value})};
+const handleSubmit = (e) => {postComment(id, info); 
+  e.preventDefault();
+    setInfo({
+      post: id,
+      content: ""
+    })
+}
+
   return (
-    <FormControl fullWidth sx={{width:"500px", margin:"1rem"}}>
+    <FormControl fullWidth sx={{width:"500px", margin:"1rem"}} component="form" onSubmit={handleSubmit}>
                   
          <TextField
           id="outlined-multiline-static"
           label="Comment"
+          name="content"
           multiline
           rows={4}
           placeholder="Add a comment"
+          value={info.content}
+          onChange={handleChange}
           InputProps={{
             startAdornment: <InputAdornment position="start"></InputAdornment>,
           }}
