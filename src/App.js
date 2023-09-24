0eacf742fe1ba7a4647154e3bee3fc27cbea2a24
aokmen <abdullah.okmen.11@gmail.com>
2023-09-24 23:15:47 +0200

    persist_redux_created_in_store_and _app_js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import { Provider } from "react-redux";
-import store from "./app/store";
 import AppRouter from "./router/AppRouter";
+import { Provider } from "react-redux";
+import store, { persistor }from "./app/store";
 import {ToastContainer} from "react-toastify"
 import  "./App.css"
 import 'react-toastify/dist/ReactToastify.css';
+import { PersistGate } from "redux-persist/integration/react";
 function App() {
 
   return (
     <>
       <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
           <AppRouter/>
+        </PersistGate>
        </Provider>
        <ToastContainer/>   
     </>
--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,13 +1,47 @@
-import {configureStore} from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/authSlice";
+// import {configureStore} from "@reduxjs/toolkit"
 import blogReducer from "../features/blogSlice"
-import authReducer from "../features/authSlice"
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+
+const persistConfig = {
+  key: "root",// storage veriler key-value şeklinde saklanır. Buraki key storagedaki keyi temsil ediyor.
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
-    reducer:{
-        blog:blogReducer,
-        auth:authReducer,
-    }
+  reducer: {
+    auth: persistedReducer,
+    blog: blogReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV !== "production",
+});
 
-})
+export let persistor = persistStore(store);
+export default store
 
-export default store;
+
+
+
+// import {configureStore} from "@reduxjs/toolkit"
+// import blogReducer from "../features/blogSlice"
+// import authReducer from "../features/authSlice"
+
+// const store = configureStore({
+//     reducer:{
+//         blog:blogReducer,
+//         auth:authReducer,
+//     }
+
+// })
+
+// export default store;
